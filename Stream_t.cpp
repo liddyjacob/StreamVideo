80ab9b2d09632d665b2a9f6e86eb2bf3d7b77757
Jacob Liddy <liddyjacob@gmail.com>
2017-12-06 23:58:03 -0500

    Update old test case for parameter data structure

--- Stream_t.cpp
+++ Stream_t.cpp
@@ -16,6 +16,12 @@ TEST_CASE( "NOP", "[Stream]" ) {
 TEST_CASE( "INITIALIZE", "[Stream]" ){
   // TODO: Perhaps a better option would be to have
   //       a single data structure that holds this information
-  Video v("Test Pilot", Video::MOVIE, 1, 58, 0);
+  VideoParams vp;
+  vp.title   = "Test Pilot";
+  vp.type    = Video::MOVIE;
+  vp.hours   = 1;
+  vp.minutes = 58;
+
+  Video v(vp);
   Stream s(v, 0);
 }
