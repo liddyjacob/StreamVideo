6b55e952c4f1cfdf66a8d8b3e045fc9a39bdc8f0
Jacob Liddy <liddyjacob@gmail.com>
2017-12-06 23:53:26 -0500

    Adds working tv series test case

--- Video_t.cpp
+++ Video_t.cpp
@@ -34,3 +34,23 @@ TEST_CASE("Video Parameters -- MOVIE", "[Video]"){
   REQUIRE(v.getMinutes() == 38);
   REQUIRE(v.getEpisodes() == 0); // as Fargo is a movie.
 }
+
+
+TEST_CASE("Video Parameters -- TV series", "[Video]"){
+  VideoParams vp;
+
+  //Fargo the TV series.
+  vp.title = "Fargo, Season Two";
+  vp.type  = Video::TVSHOW;
+  vp.hours = 0;
+  vp.minutes = 50;
+  vp.episodes = 10;  
+
+  Video v(vp);
+
+  REQUIRE(v.getTitle() == "Fargo, Season Two");
+  REQUIRE(v.getType() == 1);//Video::TVSHOW);
+  REQUIRE(v.getHours() == 0);
+  REQUIRE(v.getMinutes() == 50);
+  REQUIRE(v.getEpisodes() == 10);
+}
