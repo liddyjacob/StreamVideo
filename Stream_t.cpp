da4fc5ab8e47bd730b9780801562e58a875dcf11
ord4 <ord4@zips.uakron.edu>
2017-12-13 12:04:15 -0500

    Test case written after Stream init fails on checking the type of the Video

--- Stream_t.cpp
+++ Stream_t.cpp
@@ -25,3 +25,20 @@ TEST_CASE( "INITIALIZE", "[Stream]" ){
   Video v(vp);
   Stream s(v, 0);
 }
+
+TEST_CASE( "Stream Parameters", "[Stream]" ) {
+    VideoParams vp;
+    vp.title   = "Test Pilot";
+    vp.type    = Video::MOVIE;
+    vp.hours   = 1;
+    vp.minutes = 58;
+
+    Video v(vp);
+    Stream s(v, 0);
+ 
+    REQUIRE(s.getOccurrences() == 0);
+    REQUIRE(s.getVideo().getTitle() == "Test Pilot");
+    REQUIRE(s.getVideo().getType() == Video::MOVIE);
+    REQUIRE(s.getVideo().getHours() == 1);
+    REQUIRE(s.getVideo().getMinutes() == 58);
+}
