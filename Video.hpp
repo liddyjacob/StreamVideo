a196adc07ef22a9599092d94ec9ddb727b4542b2
Braden M. Blackwell <bmb134@zips.uakron.edu>
2017-12-13 01:36:23 -0500

    Removed dead class prototype and moved struct VideoParams before class Video

--- Video.hpp
+++ Video.hpp
@@ -9,52 +9,52 @@
 
 #include <string>
 
-class VideoParams;
+struct VideoParams
+{
+  VideoParams()
+      : title(""), type(-1), hours(0), minutes(0), episodes(0)
+  {
+  }
 
-class Video {
-public:
-
-    static const int MOVIE      = 0;
-    static const int TVSHOW     = 1;
-    static const int ORIGINAL   = 2;
-
-    // TODO: We should definately change this primative obsession
-    //         code smell.
-    // constructor
-    Video(const std::string& title, int type, int hours, int minutes, int episodes);
-    Video(const VideoParams& vp);
-
-    // video title
-    const std::string& getTitle() const;
-
-    // video type
-    int getType() const;
-
-    // length in hours
-    int getHours() const;
-
-    // length in minutes
-    int getMinutes() const;
-
-    // number of episodes
-    int getEpisodes() const;
-
-    // set video length
-    void setLength(int hours, int minutes);
-
-private:
-    std::string title;
-    int type;
-    int hours;
-    int minutes;
-    int episodes;
+  std::string title;
+  int type;
+  int hours;
+  int minutes;
+  int episodes;
 };
 
-struct VideoParams{
-  VideoParams()
-    : title(""), type(-1), hours(0),minutes(0),episodes(0)
-  { }
+class Video
+{
+public:
+  static const int MOVIE = 0;
+  static const int TVSHOW = 1;
+  static const int ORIGINAL = 2;
 
+  // TODO: We should definately change this primative obsession
+  //         code smell.
+  // constructor
+  Video(const std::string &title, int type, int hours, int minutes, int episodes);
+  Video(const VideoParams &vp);
+
+  // video title
+  const std::string &getTitle() const;
+
+  // video type
+  int getType() const;
+
+  // length in hours
+  int getHours() const;
+
+  // length in minutes
+  int getMinutes() const;
+
+  // number of episodes
+  int getEpisodes() const;
+
+  // set video length
+  void setLength(int hours, int minutes);
+
+private:
   std::string title;
   int type;
   int hours;
